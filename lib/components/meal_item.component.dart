9f3064421de762a22350db1386ddac0f1b97ffa2
emilioheinz <emiliosheinz@gmail.com>
2021-02-11 20:04:40 -0300

    finishing meal card

--- lib/components/meal_item.component.dart
+++ lib/components/meal_item.component.dart
@@ -8,12 +8,39 @@ class MealItem extends StatelessWidget {
   final MealComplexity complexity;
   final MealAffordability affordability;
 
-  MealItem(
-      {@required this.title,
-      @required this.imageUrl,
-      @required this.duration,
-      @required this.complexity,
-      @required this.affordability});
+  MealItem({
+    @required this.title,
+    @required this.imageUrl,
+    @required this.duration,
+    @required this.complexity,
+    @required this.affordability,
+  });
+
+  String get complexityText {
+    switch (complexity) {
+      case MealComplexity.Simple:
+        return 'Simple';
+      case MealComplexity.Challenging:
+        return 'Challenging';
+      case MealComplexity.Hard:
+        return 'Hard';
+      default:
+        return 'Unknown';
+    }
+  }
+
+  String get affordabilityText {
+    switch (affordability) {
+      case MealAffordability.Affordable:
+        return 'Affordable';
+      case MealAffordability.Pricey:
+        return 'Pricey';
+      case MealAffordability.Luxurious:
+        return 'Luxurious';
+      default:
+        return 'Unknown';
+    }
+  }
 
   void selectMeal() {}
 
@@ -43,7 +70,64 @@ class MealItem extends StatelessWidget {
                     fit: BoxFit.cover,
                   ),
                 ),
+                Positioned(
+                  child: Container(
+                    height: 250,
+                    decoration: BoxDecoration(
+                      gradient: LinearGradient(
+                        colors: [Colors.black, Colors.transparent],
+                        begin: Alignment.bottomCenter,
+                        end: Alignment.topCenter,
+                      ),
+                    ),
+                  ),
+                ),
+                Positioned(
+                  bottom: 20,
+                  left: 20,
+                  child: Container(
+                    width: 300,
+                    child: Text(
+                      title,
+                      softWrap: true,
+                      overflow: TextOverflow.fade,
+                      style: TextStyle(
+                        fontSize: 26,
+                        color: Colors.white,
+                      ),
+                    ),
+                  ),
+                ),
               ],
+            ),
+            Padding(
+              padding: EdgeInsets.all(20),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceAround,
+                children: [
+                  Row(
+                    children: [
+                      Icon(Icons.schedule),
+                      SizedBox(width: 6),
+                      Text('$duration min'),
+                    ],
+                  ),
+                  Row(
+                    children: [
+                      Icon(Icons.work),
+                      SizedBox(width: 6),
+                      Text(complexityText),
+                    ],
+                  ),
+                  Row(
+                    children: [
+                      Icon(Icons.money),
+                      SizedBox(width: 6),
+                      Text(affordabilityText),
+                    ],
+                  ),
+                ],
+              ),
             ),
           ],
         ),
