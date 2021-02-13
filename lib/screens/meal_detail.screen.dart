24d756701f5061389349a9951e4fcbb65aefd549
emilioheinz <emiliosheinz@gmail.com>
2021-02-13 12:49:31 -0300

    feat: ingredients to meal detail screen

--- lib/screens/meal_detail.screen.dart
+++ lib/screens/meal_detail.screen.dart
@@ -1,20 +1,62 @@
 import 'package:flutter/material.dart';
+import 'package:meals_app/dummy_data.dart';
 
 class MealDetailScreen extends StatelessWidget {
   static const routeName = '/meal-detail';
+
+  Widget buildSectionTitle(BuildContext context, String text) {
+    return Container(
+      margin: EdgeInsets.symmetric(vertical: 10),
+      child: Text(
+        text,
+        style: Theme.of(context).textTheme.headline6,
+      ),
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
     final arguments =
         ModalRoute.of(context).settings.arguments as Map<String, String>;
     final String id = arguments['id'];
+    final selectedMeal = DUMMY_MEALS.firstWhere((meal) => meal.id == id);
 
     return Scaffold(
       appBar: AppBar(
-        title: Text('Meal'),
+        title: Text(selectedMeal.title),
       ),
-      body: Center(
-        child: Text('Meal Detail $id'),
+      body: Column(
+        children: [
+          Container(
+            height: 300,
+            width: double.infinity,
+            child: Image.network(
+              selectedMeal.imageUrl,
+              fit: BoxFit.cover,
+            ),
+          ),
+          buildSectionTitle(context, 'Ingedients'),
+          Container(
+            padding: EdgeInsets.symmetric(horizontal: 30),
+            width: double.infinity,
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              children: selectedMeal.ingredients
+                  .map((ingedient) => Card(
+                        color: Theme.of(context).accentColor,
+                        child: Padding(
+                          padding:
+                              EdgeInsets.symmetric(vertical: 5, horizontal: 10),
+                          child: Text(
+                            ingedient,
+                          ),
+                        ),
+                      ))
+                  .toList(),
+            ),
+          ),
+          buildSectionTitle(context, 'Steps'),
+        ],
       ),
     );
   }
